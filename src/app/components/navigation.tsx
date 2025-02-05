af57dc689dc889b3cbd8e85c614b30c305f1c238
Abbas Radhi <abbe.radhi11@gmail.com>
2025-02-05 01:10:07 +0100

    update nav

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,52 +1,36 @@
+"use client";
+import { useEffect, useState } from "react";
+
 export const Navigation = () => {
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <nav className="w-full fixed flex items-center justify-between flex-wrap bg-blue p-6">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <span className="font-semibold text-xl tracking-tight">
-          &lt; Abbe /&gt;
-        </span>
-      </div>
-      <div className="block lg:hidden">
-        <button className="flex items-center px-3 py-2 border rounded text-white-200 border-teal-400 hover:text-white hover:border-white">
-          <svg
-            className="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
-      </div>
-      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div className="text-sm lg:flex-grow">
-          <a
-            href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white-200 hover:text-white mr-4"
-          >
-            Docs
-          </a>
-          <a
-            href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white-200 hover:text-white mr-4"
-          >
-            Examples
-          </a>
-          <a
-            href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white-200 hover:text-white"
-          >
-            Blog
-          </a>
-        </div>
-        <div>
-          <a
-            href="#"
-            className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
-          >
-            Download
-          </a>
-        </div>
+    <nav
+      className={`w-full fixed flex items-center flex-wrap ${
+        scrolling ? "bg-black" : "bg-transparent"
+      } p-6 gap-x-12 transition-colors duration-300`}
+    >
+      <span className="font-semibold text-xl tracking-tight">
+        &lt; Abbe /&gt;
+      </span>
+      <div className="">
+        <ol className="flex flex-row gap-4">
+          <li>Home</li>
+          <li>Articles</li>
+        </ol>
       </div>
     </nav>
   );
